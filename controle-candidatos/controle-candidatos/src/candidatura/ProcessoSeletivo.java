586d1c341370e3e6b9f09e12798a325ab6ae0757
Lucas Damasceno <damasceno_lucas@outlook.com>
2025-02-12 16:18:40 -0300

    :sparkles: feat: Adicionando método de seleção de candidatos com Random

--- controle-candidatos/controle-candidatos/src/candidatura/ProcessoSeletivo.java
+++ controle-candidatos/controle-candidatos/src/candidatura/ProcessoSeletivo.java
@@ -1,10 +1,33 @@
 package candidatura;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
     public static void main(String[] args) {
-        analisarCandidato(1900.0);
-        analisarCandidato(2200.0);
-        analisarCandidato(2000.0);
+        selecaoCandidatos();
+    }
+
+    static void selecaoCandidatos() {
+        String [] candidatos = {"Felipe", "Marcia", "Julia", "Paulo", "Augusto", "Monica", "Fabricio", "Mirela", "Daniela", "Jorge"};
+        int candidatosSelecionados = 0;
+        int candidatoAtual = 0;
+        double salarioBase = 2000.0;
+        while (candidatosSelecionados < 5 && candidatoAtual < candidatos.length) {
+            String candidato = candidatos[candidatoAtual];
+            double salarioPretendido = valorPretendido();
+            System.out.println("O candidato " + candidato + " pretende ganhar R$ " + salarioPretendido);
+            if (salarioBase >= salarioPretendido) {
+                System.out.println("O candidato " + candidato + " foi selecionado para a vaga");
+                candidatosSelecionados++;
+            }
+            candidatoAtual++;
+        }
+
+        System.out.println("Foram selecionados " + candidatosSelecionados + " candidatos");
+    }
+
+    static double valorPretendido() {
+        return ThreadLocalRandom.current().nextDouble(1800.0, 2200.0);
     }
 
     static void analisarCandidato(double salarioPretendido) {
