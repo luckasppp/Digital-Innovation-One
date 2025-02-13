b241e8718f617507d5f6833d42c466dfaeff5717
Lucas Damasceno <damasceno_lucas@outlook.com>
2025-02-13 17:23:50 -0300

    ✨ feat: Implementando nova exceção e classe de formatação de cep.

---
+++ controle-fluxo/controle-fluxo/src/FormatadorCepExemplo.java
@@ -0,0 +1,20 @@
+public class FormatadorCepExemplo {
+    public static void main(String[] args) {
+        try {
+            String cepFormatado = formatarCep("23765064");
+            System.out.println(cepFormatado);
+        } catch (CepInvalidoException e) {
+            // e.printStackTrace();
+            System.out.println("O CEP informado é inválido");
+        }
+    }
+
+    static String formatarCep(String cep) throws CepInvalidoException {
+        if (cep.length() != 8) {
+            throw new CepInvalidoException();
+        }
+
+        // Simulando um cep formatado
+        return "23.765-064";
+    }
+}
