344e1120a3a54316a57c48aea424c5cbc72c3eb1
Lucas Damasceno <damasceno_lucas@outlook.com>
2025-02-13 16:08:16 -0300

    :sparkles: feat: Implementando código de tratamento de exceção

--- controle-fluxo/controle-fluxo/src/AboutMe.java
+++ controle-fluxo/controle-fluxo/src/AboutMe.java
@@ -1,5 +1,35 @@
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class AboutMe {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(System.in).useLocale(Locale.US);
+            try {
+                System.out.println("Digite o seu nome");
+                String nome = scanner.next();
+
+                System.out.println("O seu sobrenome");
+                String sobrenome = scanner.next();
+
+                System.out.println("A sua idade");
+                int idade = scanner.nextInt();
+
+                System.out.println("A sua altura");
+                double altura = scanner.nextDouble();
+
+                // Imprimindo os dados obtidos do usuário
+                System.out.println("Nome: " + nome);
+                System.out.println("Sobrenome: " + sobrenome);
+                System.out.println("Idade: " + idade);
+                System.out.println("Altura: " + altura);
+            } catch (InputMismatchException e) {
+                System.out.println("Os campos idade e altura devem ser numéricos");
+            }
+        } finally {
+            scanner.close();
+        }
     }
 }
