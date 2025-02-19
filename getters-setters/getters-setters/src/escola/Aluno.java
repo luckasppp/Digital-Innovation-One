d620040edc2afd8d364875fe80ef48631f420fd2
Lucas Damasceno <damasceno_lucas@outlook.com>
2025-02-19 14:33:27 -0300

    ✨ feat: Implementando getters e setters

--- getters-setters/getters-setters/src/escola/Aluno.java
+++ getters-setters/getters-setters/src/escola/Aluno.java
@@ -1,6 +1,22 @@
 package escola;
 
 public class Aluno {
-    String nome;
-    int idade;
+    private String nome;
+    private int idade;
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String novoNome) {
+        this.nome = novoNome;
+    }
+
+    public int getIdade() {
+        return this.idade;
+    }
+
+    public void setIdade(int novaIdade) {
+        this.idade = novaIdade;
+    }
 }
