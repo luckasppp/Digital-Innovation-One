7e9860077afebda94ac92250c510864ba733e0f1
Lucas Damasceno <damasceno_lucas@outlook.com>
2025-02-19 11:36:24 -0300

    :bug: fix: Adicionando @SuppressWarnings("unused")

--- controle-fluxo/controle-fluxo/src/ExemploExcecao.java
+++ controle-fluxo/controle-fluxo/src/ExemploExcecao.java
@@ -4,6 +4,7 @@ import java.text.ParseException;
 public class ExemploExcecao {
     public static void main(String[] args) {
         try {
+            @SuppressWarnings("unused")
             Number valor = Double.valueOf("a1.75");
             valor = NumberFormat.getInstance().parse("a1.75");
         } catch (ParseException e) {
--- desafio-controle-fluxo/desafio-controle-fluxo/src/Contador.java
+++ desafio-controle-fluxo/desafio-controle-fluxo/src/Contador.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Contador {
     public static void main(String[] args) {
+        @SuppressWarnings("resource")
         Scanner terminal = new Scanner(System.in);
         System.out.println("Digite o primeiro parâmetro");
         int parametroUm = terminal.nextInt();
